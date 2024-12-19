ea4c0b3a008c150e4070f02348f6f049895964a0
debsouryadatta <debsouryadatta@gmail.com>
2024-12-20 02:42:56 +0530

    not working currently

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { YoutubeTranscript } from "youtube-transcript";
+import { getYoutubeClient, main } from './utils';
 
 export type Env = {
   YOUTUBE_API_KEY: string;
@@ -14,60 +15,10 @@ app.get('/', (c) => {
 
 app.get('/hono/youtube-transcript/:videoId', async (c) => {
   const { videoId } = c.req.param();
-  // return c.text(await getTranscript(videoId));
-  return c.json({
-    videoId,
-    transcript: await getTranscript(videoId),
-  })
+  const youtube_api_key = c.env.YOUTUBE_API_KEY;
+  const youtubeClient = getYoutubeClient(youtube_api_key);
+  const subtitles = await main(videoId, youtubeClient);
+  return c.text(subtitles!);
 })
-
-// const getTranscript = async (videoId: string, apiKey: string, oauthToken: string) => {
-//   try {
-//     // Step 1: Get caption tracks
-//     const captionsResponse = await fetch(
-//       `https://www.googleapis.com/youtube/v3/captions?videoId=${videoId}&part=snippet&key=${apiKey}`
-//     );
-    
-//     const captions = await captionsResponse.json();
-//     const captionId = captions.items[0]?.id;
-    
-//     if (!captionId) {
-//       throw new Error('No captions available');
-//     }
-
-//     // Step 2: Get the actual transcript
-//     const transcriptResponse = await fetch(
-//       `https://www.googleapis.com/youtube/v3/captions/${captionId}?key=${apiKey}`,
-//       {
-//         headers: {
-//           Authorization: `Bearer ${oauthToken}`,
-//           Accept: 'application/json'
-//         }
-//       }
-//     );
-
-//     const transcript = await transcriptResponse.json();
-//     return transcript;
-//   } catch (error) {
-//     throw new Error(`Failed to fetch transcript: ${error.message}`);
-//   }
-// }
-
-export async function getTranscript(videoId: string) {
-  try {
-    let transcript_arr = await YoutubeTranscript.fetchTranscript(videoId, {
-      // lang: "en",
-      //   @ts-ignore
-      // country: "EN",
-    });
-    let transcript = "";
-    for (let t of transcript_arr) {
-      transcript += t.text + " ";
-    }
-    return transcript.replaceAll("\n", "");
-  } catch (error) {
-    return "";
-  }
-}
 
 export default app
